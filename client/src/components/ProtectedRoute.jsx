4a5ffeda895fc67a5370cc99bbe927595d8b7a77
hengtseChou <hankthedev@gmail.com>
2025-03-23 15:47:20 +0800

    fix: use react-spinners for spinner

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { PuffLoader } from "react-spinners";
 
-import Spinner from "./Spinner";
 import { AuthContext } from "../contexts/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <Spinner />;
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <PuffLoader color="#10B981" size={150} /> {/* green 500 from tailwindcss */}
+      </div>
+    );
   }
 
   if (!isAuthenticated) {
