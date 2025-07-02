76699d3d6f6a6c8e9cdc1a6796854442041f21d1
hengtseChou <hankthedev@gmail.com>
2025-07-02 21:11:42 +0800

    fix: rename image and user module to align with endpoint

--- server/app/main.py
+++ server/app/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.auth import router as auth_router
-from app.routes.images import router as images_router
-from app.routes.users import router as users_router
+from app.routes.image import router as image_router
+from app.routes.user import router as user_router
 
 app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
 app.include_router(auth_router, prefix="/auth")
-app.include_router(images_router, prefix="/image")
-app.include_router(users_router, prefix="/user")
+app.include_router(image_router, prefix="/image")
+app.include_router(user_router, prefix="/user")
 
 origins = [
     "http://localhost:5173",
